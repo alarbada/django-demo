c43b967baec0c406f5529fe7ccce767daaaa9776
Guillem <you@example.com>
2023-04-12 20:18:47 +0200

    Add GET and POST /api/products handlers

--- api/views.py
+++ api/views.py
@@ -2,5 +2,16 @@
 # Create your views here.
 from django.http import HttpResponse, HttpRequest
 
-def index(request: HttpRequest):
+def list_products():
     return HttpResponse('{ "products": ["product1", "product2"]}')
+
+def create_product():
+    return HttpResponse(status=201)
+
+def products(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        return list_products()
+    elif request.method == "POST":
+        return create_product()
+    else:
+        return HttpResponse(status=404)
