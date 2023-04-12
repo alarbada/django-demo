d6ec1b6b9f5d39e77b1526e1b9202452aa5ba828
Guillem <you@example.com>
2023-04-12 21:15:51 +0200

    Simplify error handling

--- api/views.py
+++ api/views.py
@@ -17,51 +17,47 @@ ProductData = TypedDict("ProductData", {
 })
 
 def list_products() -> HttpResponse:
-    try:
-        products = Product.objects.get_queryset()
+    products = Product.objects.get_queryset()
 
-        productsData: List[ProductData] = []
-        for product in products:
-            data = ProductData(
-                title=product.title,
-                description=product.description,
-                price=float(product.price),
-            )
-            productsData.append(data)
+    productsData: List[ProductData] = []
+    for product in products:
+        data = ProductData(
+            title=product.title,
+            description=product.description,
+            price=float(product.price),
+        )
+        productsData.append(data)
 
-        serialized = json.dumps(productsData)
-        res = HttpResponse(serialized)
-        res["Content-Type"] = "application/json"
-        res.status_code = 200
+    serialized = json.dumps(productsData)
+    res = HttpResponse(serialized)
+    res["Content-Type"] = "application/json"
+    res.status_code = 200
 
-        return res
-
-    except:
-        return HttpResponse(status=500)
+    return res
 
 # The url does not exist, the exercise assumes that that endpoint is already implemented
 @login_required(login_url="/login")
 def create_product(req: HttpRequest) -> HttpResponse:
-    try:
-        # read request body into product
-        body = req.body.decode("utf-8")
-        data = json.loads(body)
+    # read request body into product
+    body = req.body.decode("utf-8")
+    data = json.loads(body)
 
-        product = Product()
-        product.title = data["title"]
-        product.description = data["description"]
-        product.price = Decimal(data["price"])
-        product.save()
+    product = Product()
+    product.title = data["title"]
+    product.description = data["description"]
+    product.price = Decimal(data["price"])
+    product.save()
 
-        return HttpResponse(status=201)
-    core.products
-        return HttpResponse(status=400)
+    return HttpResponse(status=201)
 
 @csrf_exempt
 def products(req: HttpRequest) -> HttpResponse:
-    if req.method == "GET":
-        return list_products()
-    elif req.method == "POST":
-        return create_product(req)
-    else:
-        return HttpResponse(status=404)
+    try:
+        if req.method == "GET":
+            return list_products()
+        elif req.method == "POST":
+            return create_product(req)
+        else:
+            return HttpResponse(status=404)
+    except:
+        return HttpResponse(status=500)
